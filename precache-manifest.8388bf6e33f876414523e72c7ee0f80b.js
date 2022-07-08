21d39a69f407566549277a0e86b1462c4521d8e7
ebra <ebrimasonko188@gmail.com>
2022-07-08 22:09:49 +0200

    Updates

---
+++ precache-manifest.8388bf6e33f876414523e72c7ee0f80b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd3b58a4d5fa1a8cc5a7",
+    "url": "/ibra188/static/css/main.14264d70.chunk.css"
+  },
+  {
+    "revision": "dd3b58a4d5fa1a8cc5a7",
+    "url": "/ibra188/static/js/main.dd3b58a4.chunk.js"
+  },
+  {
+    "revision": "af5d5329b4781fcdac6b",
+    "url": "/ibra188/static/js/1.af5d5329.chunk.js"
+  },
+  {
+    "revision": "eb28acf53be54ea56afd",
+    "url": "/ibra188/static/js/runtime~main.eb28acf5.js"
+  },
+  {
+    "revision": "23bb83a337e5674fc87df87d477ccff8",
+    "url": "/ibra188/index.html"
+  }
+];
